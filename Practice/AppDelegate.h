3a10d244d1b7588c3a124e014abc6263a4ef443e
Sohaib Muhammad <sohaib.muhammad@coeus-solutions.de>
2013-03-13 12:21:16 +0500

    Initial Commit

---
+++ Practice/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Practice
+//
+//  Created by Sohaib Muhammad on 13/03/2013.
+//  Copyright (c) 2013 coeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Practice/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Practice
+//
+//  Created by Sohaib Muhammad on 13/03/2013.
+//  Copyright (c) 2013 coeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
